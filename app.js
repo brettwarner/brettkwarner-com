722fbbd9c8a3833cdbde861f9fce94ab9003e65e
Brett Warner <bkwarner22@gmail.com>
2014-01-29 21:13:26 -0700

    Add 404 pages so the whole thing doesn't explode

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
+app.use(app.router); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -43,18 +43,59 @@ app.get('/', function(req,res){
 
 
 // This is what I'm currently working on. Will pull a post based on title.
-app.get('/posts/:postTitle', function(req,res){
+app.get('/posts/:postSlug', function(req,res){
 
-	var actualTitle = req.params.postTitle.split("-").join(" ");
-	BlogPost.findOne({ postName: actualTitle }, function(err, thePost){
-		console.log("The error"+ err);
-		console.log(thePost);
-		console.log(req.params.postTitle);
-		res.render("post", {title: thePost.postName, body: thePost.postBody});
+	BlogPost.findOne({ postSlug: req.params.postSlug }, function(err, thePost){
+		if(!thePost){
+			console.log("The error"+ err);
+			res.send('Not found');
+		}else{
+			console.log(thePost);
+			console.log(req.params.postTitle);
+			res.render("post", {title: thePost.postName, body: thePost.postBody});
+		}
 	});
 });
 //app.get("/", routes.index)
 //app.get('/users', user.list);
+
+
+// Weird Error Handling Code I stole from here:
+//
+
+// "app.router" positions our routes
+// above the middleware defined below,
+// this means that Express will attempt
+// to match & call routes _before_ continuing
+// on, at which point we assume it's a 404 because
+// no route has handled the request.
+
+// Since this is the last non-error-handling
+// middleware use()d, we assume 404, as nothing else
+// responded.
+
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+   // res.render('404', { url: req.url });
+   res.send('Not found');
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+// End of Weird Error Handling code
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
