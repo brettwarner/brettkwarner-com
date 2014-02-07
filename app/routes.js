6e7cbb3a256bdfa37a948738a7a41de29dfecd82
Brett Warner <bkwarner22@gmail.com>
2014-02-06 22:01:25 -0700

    start working on pagination

--- app/routes.js
+++ app/routes.js
@@ -24,51 +24,50 @@ module.exports = function(app){
 		});
 	});
 
-// Add a post page
+	// Add a post page
 
-app.get('/admin/createnew', ensureAuthenticated, function(req,res){
-	console.log("okay so we got this far..");
-	console.log(req + "success?");	
-	res.render("admin", {title: 'Success!', user: req.user});
-});
-app.post('/api/addcontent', ensureAuthenticated, function(req, res){
+	app.get('/admin/createnew', ensureAuthenticated, function(req,res){
+		res.render("admin", {title: 'Success!', user: req.user});
+	});
 
-	console.log("Adding Something Awesome!");
-	console.log(req.body);
-	if(req.body['content-type'] === "post"){
-		console.log("content type post");
-		BlogPost.create({
-			postName: req.body['content-title'],
-			postSlug: req.body['content-slug'],
-			postDate : new Date(),
-			postAuthor : "Brett Warner",
-			postBody: marked(req.body['content-body']),
-			postMarkdown: req.body['content-body']
-		}, function(err, success){
-			if(err)
-				res.send(err);
-			BlogPost.find(function(err, posts){
-				res.send(posts);
-			})
-		});
-	}else if(req.body['content-type'] ==="page"){
-		console.log("content type page");
-		BlogPage.create({
-			pageName: req.body['content-title'],
-			pageSlug: req.body['content-slug'],
-			pageDate : new Date(),
-			pageAuthor : "Brett Warner",
-			pageBody: marked(req.body['content-body']),
-			pageMarkdown: req.body['content-body']
-		}, function(err, success){
-			if(err)
-				res.send(err);
-			BlogPage.find(function(err,pages){
-				res.send(pages);
-			})
-		});
-	}
-});
+	app.post('/api/addcontent', ensureAuthenticated, function(req, res){
+
+		console.log("Adding Something Awesome!");
+		console.log(req.body);
+		if(req.body['content-type'] === "post"){
+			console.log("content type post");
+			BlogPost.create({
+				postName: req.body['content-title'],
+				postSlug: req.body['content-slug'],
+				postDate : new Date(),
+				postAuthor : "Brett Warner",
+				postBody: marked(req.body['content-body']),
+				postMarkdown: req.body['content-body']
+			}, function(err, success){
+				if(err)
+					res.send(err);
+				BlogPost.find(function(err, posts){
+					res.send(posts);
+				})
+			});
+		}else if(req.body['content-type'] ==="page"){
+			console.log("content type page");
+			BlogPage.create({
+				pageName: req.body['content-title'],
+				pageSlug: req.body['content-slug'],
+				pageDate : new Date(),
+				pageAuthor : "Brett Warner",
+				pageBody: marked(req.body['content-body']),
+				pageMarkdown: req.body['content-body']
+			}, function(err, success){
+				if(err)
+					res.send(err);
+				BlogPage.find(function(err,pages){
+					res.send(pages);
+				})
+			});
+		}
+	});
 
 	//Edits a piece of content needs to be done still
 	app.post("/api/editcontent", function(req, res){
@@ -126,6 +125,32 @@ app.post('/api/addcontent', ensureAuthenticated, function(req, res){
 			}
 		});
 	});
+
+	// Page number code I'm working on.
+	// Thought is to display 10 post titles per,
+	// Pass next and previous buttons through as true/false values based on numbers
+	// Pass
+
+	// app.get('/page/:pnumber', function(req, res){
+	//	var pageNumber = parseInt(req.params.pnumber);
+	//  if (typeof(pageNumber) !== integer){
+		// Return not found
+	//}else if // page number out of range
+	//return not found
+	//}else{
+		//return blog page
+//	}
+	// 	BlogPage.findOne({ pageSlug: req.params.pageSlug}, function(err, thePage){
+	// 		if(!thePage){
+	// 			console.log("the error" + err);
+	// 			res.send("Not found");
+	// 		}else{
+	// 			console.log(thePage);
+	// 			console.log(req.params.pageTitle);
+	// 			res.render("page", {title: thePage.pageName, body: thePage.pageBody});
+	// 		}
+	// 	});
+	// });
 
 	// Pulls a post based on the slug.
 	app.get('/blog/:postSlug', function(req,res){
