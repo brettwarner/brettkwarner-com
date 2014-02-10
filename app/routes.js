dde1fc22f68c380e5173175a3ffc10f903a0b606
Brett Warner <bkwarner22@gmail.com>
2014-02-09 21:41:06 -0700

    Added an edit page and API

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,9 @@
+// This is my awesomely large and disorganized routes file. 
+// If you're seeing this comment, I haven't organized it yet!
+// In my next large-scale refactor of this I'm going to split this into 2 files, one for get and one for post
+
+
+
 var BlogPost = require('./models/posts');
 var BlogPage = require('./models/pages'); 
 var marked = require('marked');
@@ -14,25 +20,27 @@ marked.setOptions({
 	smartLists: true,
 	smartypants: false
 });
-// Adds a new post!
+
 module.exports = function(app){
-	
 	app.get('/', function(req,res){
+		//Show post and page list. Homepage!
+
 		BlogPost.find(function(err, thePosts){
 			console.log(thePosts);
-			res.render("index", {title: "DinoLand", test1: "Test Here", posts: thePosts, user: req.user});
+			res.render('index', {title: 'DinoLand', test1: 'Test Here', posts: thePosts, user: req.user});
 		});
 	});
 
-	// Add a post page
-
 	app.get('/admin/createnew', ensureAuthenticated, function(req,res){
-		res.render("admin", {title: 'Success!', user: req.user});
+		// Admin Page
+
+		res.render('admin', {title: 'Success!', user: req.user});
 	});
 
 	app.post('/api/addcontent', ensureAuthenticated, function(req, res){
+		// API to Add content
 
-		console.log("Adding Something Awesome!");
+		console.log('Adding Something Awesome!');
 		console.log(req.body);
 		if(req.body['content-type'] === "post"){
 			console.log("content type post");
@@ -40,7 +48,7 @@ module.exports = function(app){
 				postName: req.body['content-title'],
 				postSlug: req.body['content-slug'],
 				postDate : new Date(),
-				postAuthor : "Brett Warner",
+				postAuthor : 'Brett Warner',
 				postBody: marked(req.body['content-body']),
 				postMarkdown: req.body['content-body']
 			}, function(err, success){
@@ -51,7 +59,7 @@ module.exports = function(app){
 				})
 			});
 		}else if(req.body['content-type'] ==="page"){
-			console.log("content type page");
+			console.log('content type page');
 			BlogPage.create({
 				pageName: req.body['content-title'],
 				pageSlug: req.body['content-slug'],
@@ -69,18 +77,9 @@ module.exports = function(app){
 		}
 	});
 
-	//Edits a piece of content needs to be done still
-	app.post("/api/editcontent", function(req, res){
-		console.log("Editing a Piece of Content!")
-	});
+app.get('/rss', function(req, res){
+		// RSS Feed
 
-	//Deletes a post needs to be done still
-	app.post("/api/editcontent", function(req, res){
-		console.log("Deleting a Post!")
-	})
-
-
-	app.get('/rss', function(req, res){
 		var feed = new Feed({
 			title: 'Brett Warner',
 			description: 'Stuff I break.',
@@ -111,17 +110,34 @@ module.exports = function(app){
         });
 	});
 
-	// Pulls a page based on the slug
-	app.get('/:pageSlug', function(req, res){
+
+app.get('/:pageSlug', function(req, res){
+		// Pulls a page based on the slug
 
 		BlogPage.findOne({ pageSlug: req.params.pageSlug}, function(err, thePage){
 			if(!thePage){
-				console.log("the error" + err);
-				res.send("Not found");
+				console.log('the error' + err);
+				res.send('Not found');
 			}else{
 				console.log(thePage);
 				console.log(req.params.pageTitle);
-				res.render("page", {title: thePage.pageName, body: thePage.pageBody});
+				res.render('page', {title: thePage.pageName, body: thePage.pageBody});
+			}
+		});
+	});
+
+
+app.get('/blog/:postSlug', function(req,res){
+		// Pulls a post based on the slug.
+
+		BlogPost.findOne({ postSlug: req.params.postSlug }, function(err, thePost){
+			if(!thePost){
+				console.log("The error"+ err);
+				res.send('Not found');
+			}else{
+				console.log(thePost);
+				console.log(req.params.postTitle);
+				res.render("post", {title: thePost.postName, body: thePost.postBody});
 			}
 		});
 	});
@@ -152,18 +168,95 @@ module.exports = function(app){
 	// 	});
 	// });
 
-	// Pulls a post based on the slug.
-	app.get('/blog/:postSlug', function(req,res){
-		BlogPost.findOne({ postSlug: req.params.postSlug }, function(err, thePost){
-			if(!thePost){
-				console.log("The error"+ err);
-				res.send('Not found');
-			}else{
-				console.log(thePost);
-				console.log(req.params.postTitle);
-				res.render("post", {title: thePost.postName, body: thePost.postBody});
-			}
-		});
+
+app.post("/api/editcontent", function(req, res){
+		//Edits a piece of content needs to be done still
+
+		console.log("Editing a Piece of Content!")
+
+		if(req.body['content-type'] === "post"){
+			console.log("This is a post!");
+
+			BlogPost.update({postSlug: req.body['content-slug']},{
+				postName: req.body['content-title'],
+				postBody: marked(req.body['content-body']),
+				postMarkdown: req.body['content-body']
+			}, function(err, thePost){
+				if(!thePost){
+					console.log("The error"+ err);
+					res.send('Post Not found');
+				}else{
+					console.log(thePost);
+					res.send('Post Updated!');
+				}
+			});
+		}else if(req.body['content-type'] ==="page"){
+			console.log("this is a page!");
+
+			BlogPage.update({pageSlug: req.body['content-slug']},{
+				pageName: req.body['content-title'],
+				pageBody: marked(req.body['content-body']),
+				pageMarkdown: req.body['content-body']
+			}, function(err, thePage){
+				if(!thePage){
+					console.log("The error"+ err);
+					res.send('Page Not found');
+				}else{
+					console.log(thePage);
+					res.send('Page Updated!');
+				}
+			});
+
+		}else{
+			console.log("Unknown content type");
+			res.send("Type Not Found!");
+		}
+		
+	});
+
+
+app.post("/api/delete", function(req, res){
+		//Deletes a post needs to be done still
+
+		console.log("Deleting a Post!")
+	});
+
+
+app.get('/admin/edit/:type-:slug', ensureAuthenticated, function(req, res){
+		// Edits a post or page based on type passed through!
+
+		console.log(req.params.type);
+		console.log(req.params.slug);
+		if(req.params.type === 'post'){
+			console.log('This is a post!');
+
+			BlogPost.findOne({postSlug: req.params.slug}, function(err, thePost){
+				if(!thePost){
+					console.log("The error"+ err);
+					res.send('Post Not found');
+				}else{
+					console.log(thePost);
+					res.render("editor", {title: thePost.postName, content: thePost.postMarkdown, slug: thePost.postSlug, type: 'post'});
+				}
+			});
+
+		}else if(req.params.type ==='page'){
+			console.log('this is a page!');
+
+			BlogPage.findOne({pageSlug: req.params.slug}, function(err, thePage){
+				if(!thePage){
+					console.log('The error'+ err);
+					res.send('Page Not found');
+				}else{
+					console.log(thePage);
+					res.render('editor', {title: thePage.pageName, content: thePage.pageMarkdown, slug: thePage.pageSlug, type: 'page'  });
+				}
+			});
+
+		}else{
+			console.log('Unknown content type');
+			res.send('Type Not Found!');
+		}
 	});
 }
 
