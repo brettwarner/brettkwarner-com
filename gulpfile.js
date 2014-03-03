ffb1b777056f7466f1c360aed1fda8bc65f09eee
Brett Warner <bkwarner22@gmail.com>
2014-03-02 18:44:53 -0700

    add new structure, gulp, and mocha

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('default', ['lint']);
+gulp.task('lint', function(){
+	gulp.src('./src/**/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+
+});
