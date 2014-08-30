1d9b582f0c251e57095a0d3556df33876e820f47
Brett Warner <bkwarner22@gmail.com>
2014-08-30 08:53:57 -0700

    fixes tabs in config-sample

--- config-sample.js
+++ config-sample.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   siteTitle: 'My Awesome Blog',
-	siteAuthor: 'YOUR NAME',
-	siteUrl: 'http://www.warbrett.com',
+  siteAuthor: 'YOUR NAME',
+  siteUrl: 'http://www.warbrett.com',
   siteAuthorEmail: 'you@email.com',
   siteDescription: 'Description of the site goes here.',
   databaseUrl: process.env.DATABASE_URL
